f26890b4c7e5f87a090d8ec265d2e2138371aeef
andylu0129 <andylu19980101@gmail.com>
2026-02-16 12:10:32 +1100

    included password matching logic in zod signUpFormSchema and updated sign up form with the new react-hook-form and zod validations

--- src/lib/zod/schemas/form-schema.ts
+++ src/lib/zod/schemas/form-schema.ts
@@ -1,23 +1,38 @@
 import z from 'zod';
 
-const passwordRule = z
-  .string()
-  .min(8, 'Password must be at least 8 characters')
-  .max(64, 'Password must be at most 64 characters')
-  .regex(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).*$/,
-    'Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character',
+export const signUpFormSchema = z
+  .object({
+    firstName: z.string().min(1, 'First name is required'),
+    lastName: z.string().min(1, 'Last name is required'),
+    email: z.email(),
+    password: z
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .max(64, 'Password must be at most 64 characters')
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).*$/,
+        'Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character',
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.confirmPassword;
+    },
+    {
+      message: 'Passwords do not match',
+      path: ['confirmPassword'],
+    },
   );
-
-export const signUpFormSchema = z.object({
-  firstName: z.string().min(1, 'First name is required'),
-  lastName: z.string().min(1, 'Last name is required'),
-  email: z.email(),
-  password: passwordRule,
-  confirmPassword: passwordRule,
-});
 
 export const signInFormSchema = z.object({
   email: z.email(),
-  password: passwordRule,
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(64, 'Password must be at most 64 characters')
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).*$/,
+      'Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character',
+    ),
 });
