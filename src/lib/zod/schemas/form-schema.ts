ac23726dea4631a60a5bf72e6a0c25f1540fc6ca
andylu0129 <andylu19980101@gmail.com>
2026-02-16 12:29:39 +1100

    added back regex password validation for the zod signInFormSchema for UX purpose

--- src/lib/zod/schemas/form-schema.ts
+++ src/lib/zod/schemas/form-schema.ts
@@ -30,5 +30,9 @@ export const signInFormSchema = z.object({
   password: z
     .string()
     .min(8, 'Password must be at least 8 characters')
-    .max(64, 'Password must be at most 64 characters'),
+    .max(64, 'Password must be at most 64 characters')
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).*$/,
+      'Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character',
+    ),
 });
